4fe82e1af229e43b545bb3c1e67f3a2f3a3db568
masahiro yamada <mayama4u@gmail.com>
2012-07-22 17:13:36 +0900

    Before DB connect

---
+++ fuel/app/logs/2012/07/22.php
@@ -0,0 +1,18 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+Error - 2012-07-22 07:22:59 --> Error - Call to undefined method Fuel\Core\Request::isPost() in /Applications/MAMP/htdocs/blog/fuel/app/classes/controller/account.php on line 52
+Error - 2012-07-22 07:25:45 --> Error - Call to undefined method Controller_Account::forward404() in /Applications/MAMP/htdocs/blog/fuel/app/classes/controller/account.php on line 53
+Error - 2012-07-22 07:26:59 --> Error - Call to undefined method Fuel\Core\Request::getPost() in /Applications/MAMP/htdocs/blog/fuel/app/classes/controller/account.php on line 56
+Error - 2012-07-22 07:29:04 --> Error - Call to undefined method Fuel\Core\Request::getPost() in /Applications/MAMP/htdocs/blog/fuel/app/classes/controller/account.php on line 69
+Error - 2012-07-22 07:31:31 --> 8 - Undefined property: Controller_Account::$db_manager in /Applications/MAMP/htdocs/blog/fuel/app/classes/controller/account.php on line 80
+Error - 2012-07-22 07:31:31 --> Error - Call to a member function get() on a non-object in /Applications/MAMP/htdocs/blog/fuel/app/classes/controller/account.php on line 80
+Error - 2012-07-22 07:41:25 --> 8 - Undefined property: Controller_Account::$db_manager in /Applications/MAMP/htdocs/blog/fuel/app/classes/controller/account.php on line 81
+Error - 2012-07-22 07:41:25 --> Error - Call to a member function get() on a non-object in /Applications/MAMP/htdocs/blog/fuel/app/classes/controller/account.php on line 81
+Error - 2012-07-22 07:42:08 --> Error - Call to undefined method Controller_Account::render() in /Applications/MAMP/htdocs/blog/fuel/app/classes/controller/account.php on line 101
+Error - 2012-07-22 07:44:47 --> Error - Using $this when not in object context in /Applications/MAMP/htdocs/blog/fuel/app/views/account/signup.php on line 9
+Error - 2012-07-22 07:47:31 --> Error - Using $this when not in object context in /Applications/MAMP/htdocs/blog/fuel/app/views/errors.php on line 3
+Error - 2012-07-22 07:48:38 --> Error - Class 'UserRepository' not found in /Applications/MAMP/htdocs/blog/fuel/app/classes/dbmanager.php on line 97
+Error - 2012-07-22 07:51:54 --> 2 - require(/Applications/MAMP/htdocs/blog/fuel/app/classses/model/UserRepository.php) [<a href='function.require'>function.require</a>]: failed to open stream: No such file or directory in /Applications/MAMP/htdocs/blog/fuel/app/classes/dbmanager.php on line 97
+Error - 2012-07-22 07:51:54 --> Compile Error - require() [<a href='function.require'>function.require</a>]: Failed opening required '/Applications/MAMP/htdocs/blog/fuel/app/classses/model/UserRepository.php' (include_path='.:/Applications/MAMP/bin/php/php5.3.6/lib/php') in /Applications/MAMP/htdocs/blog/fuel/app/classes/dbmanager.php on line 97
+Error - 2012-07-22 07:52:41 --> Error - Class 'DbRepository' not found in /Applications/MAMP/htdocs/blog/fuel/app/classes/model/UserRepository.php on line 9
+Error - 2012-07-22 07:54:33 --> Error - Call to a member function prepare() on a non-object in /Applications/MAMP/htdocs/blog/fuel/app/classes/dbrepository.php on line 41
